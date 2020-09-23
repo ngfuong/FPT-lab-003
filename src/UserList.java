dbe7fbb60d8e7a73bba44834d83f05e1e71ff4ed
ngfuong-XPS-Ubuntu <lenguyenphuong2204@gmail.com>
2020-09-23 17:04:58 +0700

    Commit #2: Modified UserList - No test

--- src/UserList.java
+++ src/UserList.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.util.Base64;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class UserList extends ArrayList<User> {
     public UserList() {
@@ -125,7 +123,6 @@ public class UserList extends ArrayList<User> {
     //add new user
     public boolean addUser() {
         String username, fName, lName, password, confirm, phone, email;
-        boolean operation = true;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter new username:");
@@ -134,7 +131,6 @@ public class UserList extends ArrayList<User> {
             System.out.println("Error: Username existed!");
             return false;
         }
-
 
         System.out.print("Enter first name:");
         fName = sc.nextLine();
@@ -163,6 +159,7 @@ public class UserList extends ArrayList<User> {
             System.out.println("Error: Password does not match!");
             return false;
         }
+        else password = confirm = encrypt(password);
 
         System.out.println("Enter phone: ");
         phone = sc.nextLine();
@@ -178,16 +175,17 @@ public class UserList extends ArrayList<User> {
             return false;
         }
 
-        return operation;
+        this.add(new User(username, fName, lName, password, confirm, phone, email));
+
+        return true;
     }
 
     //check exist
     public boolean checkExist() {
-        String username;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter username to check: ");
-        username = sc.nextLine();
+        String username = sc.nextLine();
 
         if (!checkNull(username)) {
             System.out.println("Error: Input cannot be null!");
@@ -198,49 +196,61 @@ public class UserList extends ArrayList<User> {
     }
 
     //search information by name
+    //output list of user info
     public boolean searchName() {
         Scanner sc = new Scanner(System.in);
         String name;
-        boolean operation = true;
-        int pos=-1;
+
+        if (this.isEmpty()) {
+            System.out.println("Error: List empty!");
+            return false;
+        }
 
         System.out.println("Enter name to search: ");
         name = sc.nextLine();
         if (!checkNull(name)) {
             System.out.println("Error: Input cannot be null!");
-            return operation = false;
+            return false;
         }
+
+        ArrayList<User> list = new ArrayList<>();
 
         for (int i=0;i<this.size();i++) {
-            String fullname = this.get(i).getfName() + " "
-                                + this.get(i).getlName();
-            if (fullname.contains(name)) {
-                pos = i;
-                break;
-            }
+            String fullName = this.get(i).getfName() + " " + this.get(i).getlName();
+            if (fullName.contains(name)) list.add(this.get(i));
         }
+        //sort by first name
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                if (user1.fName.compareTo(user2.fName)>0) return 1;
+                if (user1.fName.compareTo(user2.fName)<0) return -1;
+                return 0;
+            }
+        });
 
-        if (pos==-1) {
+        if (list.isEmpty()) {
             System.out.println("No user found!");
-            return operation = false;
+            return false;
         }
         else {
             System.out.println("User information: ");
-            this.get(pos).toString();
+            for (User user: this)
+                user.toString();
         }
-        return operation;
+
+        return true;
     }
 
     //update user - update
     public boolean updateUser() {
         Scanner sc = new Scanner(System.in);
         String fName, lName, password, confirm, phone, email;
-        boolean operation = true;
 
         int pos = login();
         if (pos==-1) {
             System.out.println("Error: Login failed!");
-            return operation = false;
+            return false;
         }
 
         System.out.println("Update first name: ");
@@ -251,114 +261,108 @@ public class UserList extends ArrayList<User> {
 
         System.out.println("Update password: ");
         password = sc.nextLine();
-        if (!checkPassword(password)) {
-            System.out.println("Error: Password must be 6-char long and contains no space!");
-            return operation = false;
-        }
-
+        //update password
         if (checkNull(password)) {
+            if (!checkPassword(password)) {
+                System.out.println("Error: Password must have over characters and contain no space!");
+                return false;
+            }
+
             System.out.println("Confirm new password: ");
             confirm = sc.nextLine();
             if (!confirm.equals(password)) {
                 System.out.println("Error: Password does not match!");
-                return operation = false;
+                return false;
             }
+            else password = encrypt(password);
         }
 
         System.out.println("Update phone: ");
         phone = sc.nextLine();
+        //update phone
         if (checkNull(phone)&&checkPhone(phone)) {
             System.out.println("Error: Invalid number!");
-            return operation = false;
+            return false;
         }
 
         System.out.println("Update email: ");
         email = sc.nextLine();
+        //update email
         if (checkNull(email)&&checkEmail(email)) {
             System.out.println("Error: Invalid email!");
-            return operation = false;
+            return false;
         }
 
-        if (operation==true) {
-            if (checkNull(fName)) this.get(pos).setfName(fName);
-            if (checkNull(lName)) this.get(pos).setlName(lName);
-            if (checkNull(password)) this.get(pos).setPassword(password);
-            if (checkNull(phone)) this.get(pos).setPhone(phone);
-            if (checkNull(email)) this.get(pos).setEmail(email);
+        //update
+        if (checkNull(fName)) this.get(pos).setfName(fName);
+        if (checkNull(lName)) this.get(pos).setlName(lName);
+        if (checkNull(password)) {
+            this.get(pos).setPassword(password);
+            this.get(pos).setConfirm(password);
         }
-        return operation;
+        if (checkNull(phone)) this.get(pos).setPhone(phone);
+        if (checkNull(email)) this.get(pos).setEmail(email);
+        return true;
     }
 
     //update user - delete
     public boolean deleteUser() {
         Scanner sc = new Scanner(System.in);
-        boolean operation = true;
-        String username;
 
         int pos = login();
         if (pos==-1) {
             System.out.println("Error: Login failed!");
-            return operation = false;
+            return false;
         }
 
-        System.out.println("Delete username: ");
-        username = sc.nextLine();
+        System.out.println("Confirm delete? [Y/n]");
+        if (sc.nextLine()=="Y") this.remove(pos);
+        else return false;          //do nothing
 
-        if (checkNull(username)) {
-            System.out.println("Error: Input cannot be null!");
-            return operation = false;
-        }
-
-        if (search(username)==-1) {
-            System.out.println("Username not found!");
-            return operation = false;
-        }
-
-        if (operation == true) this.remove(pos);
-        return operation;
+        return true;
     }
 
     //write list to file
     public boolean writeFile() {
-        boolean operation = true;
-        Scanner sc = new Scanner(System.in);
         String path = "..\\user.txt";
-        String line;
 
         try {
             FileWriter writer = new FileWriter(path);
 
             int i=0;
             while (!this.isEmpty()) {
-                writer.write(this.get(i++).toString());
+                //write username only
+                writer.write(this.get(i++).getUsername());
             }
+
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
-            return operation = false;
+            return false;
         }
 
-        return operation;
+        return true;
     }
 
     //print list in file
     public boolean printFile() {
-        boolean operation = true;
         String path = "..\\user.txt";
 
         try {
             File f = new File(path);
             Scanner reader = new Scanner(f);
+
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
                 System.out.println(line);
             }
+
             reader.close();
         } catch (Exception e) {
             e.printStackTrace();
-            operation = false;
+            return false;
         }
-        return operation;
+        return true;
     }
     /**END OF OPTIONS*/
 }
